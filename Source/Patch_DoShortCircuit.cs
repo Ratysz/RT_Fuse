852b3a1cdb85568ead62b6f69485065a703d65dc
Ratys <ratysz@gmail.com>
2018-08-29 10:50:08 +0300

    Updated to B19; fixed #8, #9.

--- Source/Patch_DoShortCircuit.cs
+++ Source/Patch_DoShortCircuit.cs
@@ -29,6 +29,7 @@ namespace RT_Fuse
 			float totalEnergy = 0f;
 			float totalEnergyHistoric = 0f;
 			float explosionRadius = 0f;
+			bool shouldStartFire = false;
 			bool startedFire = false;
 			string culpritString;
 
@@ -52,7 +53,19 @@ namespace RT_Fuse
 			}
 			else
 			{
-				startedFire = TryStartFireNear(culprit);
+				shouldStartFire = true;
+				bool mitigated = false;
+				foreach (CompPower transmitter in powerNet.transmitters)
+				{
+					CompRTFuse fuseComp = transmitter.parent.GetComp<CompRTFuse>();
+					if (fuseComp != null)
+					{
+						fuseComp.MitigateSurge(fuseComp.surgeMitigation);
+						mitigated = true;
+						break;
+					}
+				}
+				startedFire = !mitigated && TryStartFireNear(culprit);
 			}
 
 			if (culprit.def == ThingDefOf.PowerConduit)
@@ -65,12 +78,22 @@ namespace RT_Fuse
 			}
 
 			StringBuilder stringBuilder = new StringBuilder();
-			if (startedFire)
+			if (shouldStartFire)
 			{
-				stringBuilder.Append("ShortCircuitStartedFire".Translate(new object[]
+				if (startedFire)
 				{
-					culpritString
-				}));
+					stringBuilder.Append("ShortCircuitStartedFire".Translate(new object[]
+					{
+						culpritString
+					}));
+				}
+				else
+				{
+					stringBuilder.Append("ShortCircuit".Translate(new object[]
+					{
+						culpritString
+					}));
+				}
 			}
 			else
 			{
@@ -78,61 +101,61 @@ namespace RT_Fuse
 				{
 					culpritString
 				}));
-			}
-			if (totalEnergy > 0f)
-			{
-				explosionRadius = Mathf.Sqrt(totalEnergy) * 0.05f;
-				explosionRadius = Mathf.Clamp(explosionRadius, 1.5f, 14.9f);
-				GenExplosion.DoExplosion(culprit.Position, powerNet.Map, explosionRadius, DamageDefOf.Flame, null, -1, null, null, null, null, null, 0f, 1, false, null, 0f, 1, 0f, false);
-				if (explosionRadius > 3.5f)
+				if (totalEnergy > 0f)
 				{
-					GenExplosion.DoExplosion(culprit.Position, powerNet.Map, explosionRadius * 0.3f, DamageDefOf.Bomb, null, -1, null, null, null, null, null, 0f, 1, false, null, 0f, 1, 0f, false);
-				}
-
-				if (totalEnergy == totalEnergyHistoric)
-				{
-					stringBuilder.AppendLine();
-					stringBuilder.AppendLine();
-					stringBuilder.Append("ShortCircuitDischargedEnergy".Translate(new object[]
+					explosionRadius = Mathf.Sqrt(totalEnergy) * 0.05f;
+					explosionRadius = Mathf.Clamp(explosionRadius, 1.5f, 14.9f);
+					GenExplosion.DoExplosion(culprit.Position, powerNet.Map, explosionRadius, DamageDefOf.Flame, null, -1, -1f, null, null, null, null, null, 0f, 1, false, null, 0f, 1, 0f, false);
+					if (explosionRadius > 3.5f)
 					{
+						GenExplosion.DoExplosion(culprit.Position, powerNet.Map, explosionRadius * 0.3f, DamageDefOf.Bomb, null, -1, -1f, null, null, null, null, null, 0f, 1, false, null, 0f, 1, 0f, false);
+					}
+
+					if (totalEnergy == totalEnergyHistoric)
+					{
+						stringBuilder.AppendLine();
+						stringBuilder.AppendLine();
+						stringBuilder.Append("ShortCircuitDischargedEnergy".Translate(new object[]
+						{
 						totalEnergyHistoric.ToString("F0")
-					}));
+						}));
+					}
+					else
+					{
+						stringBuilder.AppendLine();
+						stringBuilder.AppendLine();
+						stringBuilder.Append("IncidentWorker_RTShortCircuit_PartialMitigation".Translate(new object[]
+						{
+						totalEnergyHistoric.ToString("F0"),
+						(totalEnergyHistoric - totalEnergy).ToString("F0")
+						}));
+					}
 				}
 				else
 				{
+					culprit.TakeDamage(new DamageInfo(
+						DamageDefOf.Bomb, Rand.Range(0, (int)Math.Floor(0.1f * culprit.MaxHitPoints)),
+						0f, -1f, null, null));
+
 					stringBuilder.AppendLine();
 					stringBuilder.AppendLine();
-					stringBuilder.Append("IncidentWorker_RTShortCircuit_PartialMitigation".Translate(new object[]
+					stringBuilder.Append("IncidentWorker_RTShortCircuit_FullMitigation".Translate(new object[]
 					{
-						totalEnergyHistoric.ToString("F0"),
-						(totalEnergyHistoric - totalEnergy).ToString("F0")
+					totalEnergyHistoric.ToString("F0")
 					}));
 				}
-			}
-			else
-			{
-				culprit.TakeDamage(new DamageInfo(
-					DamageDefOf.Bomb, Rand.Range(0, (int)Math.Floor(0.1f * culprit.MaxHitPoints)),
-					-1f, null, null));
-
-				stringBuilder.AppendLine();
-				stringBuilder.AppendLine();
-				stringBuilder.Append("IncidentWorker_RTShortCircuit_FullMitigation".Translate(new object[]
+				if (explosionRadius > 5f)
 				{
-					totalEnergyHistoric.ToString("F0")
-				}));
-			}
-			if (explosionRadius > 5f)
-			{
-				stringBuilder.AppendLine();
-				stringBuilder.AppendLine();
-				stringBuilder.Append("ShortCircuitWasLarge".Translate());
-			}
-			if (explosionRadius > 8f)
-			{
-				stringBuilder.AppendLine();
-				stringBuilder.AppendLine();
-				stringBuilder.Append("ShortCircuitWasHuge".Translate());
+					stringBuilder.AppendLine();
+					stringBuilder.AppendLine();
+					stringBuilder.Append("ShortCircuitWasLarge".Translate());
+				}
+				if (explosionRadius > 8f)
+				{
+					stringBuilder.AppendLine();
+					stringBuilder.AppendLine();
+					stringBuilder.Append("ShortCircuitWasHuge".Translate());
+				}
 			}
 			Find.LetterStack.ReceiveLetter("LetterLabelShortCircuit".Translate(), stringBuilder.ToString(), LetterDefOf.NegativeEvent, new TargetInfo(culprit.Position, map, false), null);
 			return false;
