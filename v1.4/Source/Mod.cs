635e029258fb68eb794cffe52e0bc1de579b36f9
Ratys <alexander.sepity@gmail.com>
2022-10-21 19:25:47 -0500

    Updated to RimWorld 1.4, fixed up Mod Manager manifest.

---
+++ v1.4/Source/Mod.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+namespace RT_Fuse
+{
+	internal class Mod : Verse.Mod
+	{
+		public Mod(ModContentPack content) : base(content)
+		{
+			var harmony = new Harmony("io.github.ratysz.madskills");
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+		}
+	}
+}
